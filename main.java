66b467540ff47f892fc542d420bbba7e2c1047e3
Robin Steffen <robin.steffen@stud.hs-hannover.de>
2026-05-12 08:49:34 +0200

    summe implementiert

--- main.java
+++ main.java
@@ -3,6 +3,9 @@ public class main
 {
     public static void main()
     {
-        
+        int summe = 0;
+        for(int i = 1; i >= 20; i++){
+            summe = summe + i;
+        }
     }
 }
