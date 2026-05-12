631ba248c4ab1d3ecc237d1a38e25e53a8e4bd94
Robin Steffen <robin.steffen@stud.hs-hannover.de>
2026-05-12 08:54:38 +0200

    Multiplikation wurde umgesetzt

--- main.java
+++ main.java
@@ -4,8 +4,13 @@ public class main
     public static void main()
     {
         int summe = 0;
-        for(int i = 1; i >= 20; i++){
+        for(int i = 1; i <= 20; i++){
             summe = summe + i;
+        }
+        
+        int mult = 1;
+        for (int i = 1; i <= 10; i++){
+            mult = mult * i;
         }
     }
 }
